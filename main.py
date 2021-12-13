c94314e6498826f2f0fef1dd184b6bb3962f4f59
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-13 10:56:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
-
+from Script import script
 import requests 
 
 import os
 
 
-API = "https://apis.xditya.me/lyrics?song="
 
 
 Ek = Client(
@@ -18,61 +17,85 @@ Ek = Client(
 
 
 @Ek.on_message(filters.private & filters.command(["start"]))
-async def start(bot, update):
-    TEXT = "Hai {} \n\n**I Am Lyrics Search Bot. Send Me A Song Name, I Will Give You The Lyrics. ** \n\nFor Know More /help"
-    BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("Channel 🔰", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("Support Group ⭕️", url = "https://telegram.me/ekbotz_support")],[InlineKeyboardButton("Repo 🗂️", url = "https://github.com/M-fazin/Lyrics-Search-Bot"),InlineKeyboardButton("Deploy 🗃️", url = "https://heroku.com/deploy?template=https://github.com/M-fazin/Lyrics-Search-Bot")],[InlineKeyboardButton("Developer 💡", url = "https://github.com/M-fazin/")]])
-    await update.reply_text(
-        text=TEXT.format(update.from_user.mention),
-        reply_markup=BUTTONS,
-        disable_web_page_preview=True,
-        quote=True
-    )
-	
-@Ek.on_message(filters.private & filters.command(["help"]))
-async def help(bot, update):
-    HELP = "Hai {} \n\n**There Is Nothing To Know More.** \n- Send Me A Song Name, I Will Give Lyrics Of That Song. \nBot By @EKBOTZ_UPDATE "
-    HELP_BUTTON = InlineKeyboardMarkup([[InlineKeyboardButton("🧑‍💻 Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]])
-    await update.reply_text(
-        text=HELP.format(update.from_user.mention),
-        reply_markup=HELP_BUTTON,
-        disable_web_page_preview=True,
-        quote=True
+   async def start(client, message):
+       buttons = [[                                    
+            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
+            InlineKeyboardButton('😊 About', callback_data='about')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode='html'
         )
-	
-@Ek.on_message(filters.private & filters.command(["about", "source", "repo"]))
-async def about(bot, update):
-    ABOUT = "**🤖 Bot :** Lyrics Search Bot\n\n**🧑‍💻 Developer :** [M-fazin](https://github.com/M-fazin)\n\n**💻 Channel :** @EKBOTZ_UPDATE\n\n**☎️ Support :** @ekbotz_support \n\n**🗂️ Source Code :** [Lyrics Search Bot](https://github.com/M-fazin/Lyrics-Search-Bot)\n\n**⚙️ Language :** Python 3\n\n**🛡️ Framework :** Pyrogram"
-    await update.reply_text(
-	text=ABOUT,
-	disable_web_page_preview=True,
-	quote=True
-	)
-
-@Ek.on_message(filters.private & filters.text)
-async def sng(bot, message):
-        hy = await message.reply_text("`Searching 🔎`")
-        song = message.text
-        chat_id = message.from_user.id
-        rpl = lyrics(song) 
-        try:
-                await hy.delete()
-                await Ek.send_message(chat_id, text = rpl, reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Dev 🔗 ", url = f"github.com/M-fazin")], [InlineKeyboardButton("🧑‍💻 Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]]))
-        except requests.ConnectionError as exception:
-        	await hy.delete()
-        	await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🧑‍💻 Developer", url = f"github.com/M-fazin")], [InlineKeyboardButton("🧑‍💻 Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]]))
-
-
-def search(song):
-        r = requests.get(API + song)
-        find = r.json()
-        return find
-       
-def lyrics(song):
-        fin = search(song)
-        text = f'**🎶 Successfully Extracted Lyrics Of {song} 🎶**\n\n\n\n'
-        text += f'`{fin["lyrics"]}`'
-        text += '\n\n\n**Made With ❤️ By @EKBOTZ_UPDATE**'
-        return text
+    elif query.data == "help":
+        buttons = [[
+            InlineKeyboardButton('🎧MUSIC🎧', callback_data='music'),
+            InlineKeyboardButton('📀VSONG📀', callback_data='vsong')
+            ],[
+            InlineKeyboardButton('🎶LYRICS🎶', callback_data='lyrics')
+            ],[
+            InlineKeyboardButton('🏠 Home', callback_data='start'),            
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.HELP_TXT.format(query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+    elif query.data == "about":
+        buttons= [[            
+            InlineKeyboardButton('♥️ Source', callback_data='source')
+            ],[
+            InlineKeyboardButton('🏠 Home', callback_data='start'),
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.ABOUT_TXT.format(temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+    elif query.data == "source":
+        buttons = [[
+            InlineKeyboardButton('👩‍🦯 Back', callback_data='about')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.SOURCE_TXT,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+    elif query.data == "music":
+        buttons = [[
+            InlineKeyboardButton('👩‍🦯 Back', callback_data='help'),           
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.MUSIC,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )            
+    elif query.data == "vsong":
+        buttons = [[
+            InlineKeyboardButton('👩‍🦯 Back', callback_data='help')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.VSONG,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+    elif query.data == "lyrics":
+        buttons = [[
+            InlineKeyboardButton('👩‍🦯 Back', callback_data='help')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.LYRICS,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
 
 
 Ek.run()
